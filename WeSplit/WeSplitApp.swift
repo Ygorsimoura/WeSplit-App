df6638acbb3e7551c7f0fde5a301c2778c741350
ygorsimoura <ygorsimoura01@gmail.com>
2023-09-02 09:16:02 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ygor Simoura on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
